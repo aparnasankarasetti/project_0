a16d462c9ef4e94e9601c970f08440fb5019cc4d
aparnasankarasetti <87164790+aparnasankarasetti@users.noreply.github.com>
2021-07-12 10:08:17 +0530

    Create meal_info.sql

---
+++ meal_info.sql
@@ -0,0 +1,17 @@
+SELECT * FROM meal_info;
+-- using select we can select one row from a table
+DELETE from table
+SELECT * FROM table WHERE meal_id = 1438;
+-- delete one row from a table
+SELECT * FROM meal_info limit 1;
+-- using limit we can selected limited rows from a table
+UPDATE table
+SET category = "salad" WHERE  meal_id = 2307;
+SELECT * FROM TABLE;
+-- using update we can update the values in a table
+INSERT into table values(1755,"Beverages","thai");
+SELECT * FROM table;
+-- using insert we give values into a table
+SELECT * FROM table ORDER BY category  DESC;
+DROP meal_info;
+-- delete the database
